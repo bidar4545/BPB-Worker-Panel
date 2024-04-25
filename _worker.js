6e4e153ac86ae5f702620efa15c3b32d3a7c7fe9
bidar4545 <119541367+bidar4545@users.noreply.github.com>
2024-04-25 18:34:14 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'f5d8eaa1-32cd-4ccd-881e-25e7ca5e2b23';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
